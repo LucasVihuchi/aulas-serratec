46d01b5dab55a30e4e7c6702168722b2c73ce46f
Lucas Vihuchi <lucasvihuchi@gmail.com>
2022-04-25 21:16:59 -0300

    Aula 8 pt 2

---
+++ Aula8/src/br/com/serratec/exemploexcecoes/TratamentoExcecoesMainComThrowsELeitorSOLID.java
@@ -0,0 +1,57 @@
+package br.com.serratec.exemploexcecoes;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TratamentoExcecoesMainComThrowsELeitorSOLID {
+
+	public static void main(String[] args) {
+		try {
+			Scanner leitor = new Scanner(System.in);
+
+			int numero1 = lerInteiro(leitor, "Insira o primeiro número: ");
+
+			leitor.close();
+			int numero2 = lerInteiro(leitor, "Insira o segundo número: ");
+
+			// Tratamento para divisão por 0
+			try {
+				double resultado = dividir(numero1, numero2);
+				System.out.println("Resultado divisão: " + resultado);
+			} catch (ArithmeticException excecaoMat) {
+				System.out.println("Valor inválido. Texto da Exceção: " + excecaoMat.getMessage());
+			}
+		}
+		catch (IllegalStateException iStateExc) {
+			fecharPrograma("\nO scanner está fechado. Não consigo continuar :(");			
+		}
+		
+	}
+
+	public static int dividir(int n1, int n2) throws ArithmeticException {
+		return n1 / n2;
+	}
+	
+	public static void fecharPrograma(String textoFechamentoPrograma) {
+		System.out.println(textoFechamentoPrograma);
+		System.exit(1);
+	}
+	
+	public static int lerInteiro(Scanner leitor, String mensagemLeitura) {
+		int numero = 0;
+		do {
+			System.out.print(mensagemLeitura);
+			try {
+				numero = leitor.nextInt();
+				break;
+			} catch (InputMismatchException ie) {
+				System.out.println("Insira um número. Outros caracteres não são aceitos");
+			} finally {
+				// Executado se a exceção estourar ou não
+				leitor.nextLine();
+			}
+		} while (true);
+		return numero;
+	}
+
+}
